e021d6f33c327cafdabd55761a3778b49f2b6355
Monkey_G <56499092+Monkey-G@users.noreply.github.com>
2022-11-13 22:50:21 +0800

    Update LeetCode0009.cpp

--- LeetCode0009.cpp
+++ LeetCode0009.cpp
@@ -16,9 +16,69 @@ public:
         else
         if (x > 0) {
             int n = log10(x);
-            queue<int> qu;
             LL x_reverse = 0;
-            EFOR(i, 0, n) x_reverse += ((LL)x % (LL)pow(10, i+1)) / (LL)pow(10, i) * (LL)pow (10, n-i);
+            LL tmp = x;
+            EFOR(i, 0, n) {x_reverse = x_reverse * 10 + tmp % 10; tmp /= 10;}
+            return (LL)x == x_reverse;
+        }
+        else return false;
+    }
+};
+
+
+//加速
+int __FAST_IO__ = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    return 0;
+}();
+
+class Solution02 {//官方解，只翻转一半数
+public:
+    bool isPalindrome(int x) {
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
+
+using LL = long long;
+
+#define EFOR(i, m, n) for (int i = m; i <= n; ++i)
+
+//加速
+int __FAST_IO__ = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    return 0;
+}();
+
+class Solution03 {//自己写的另一个傻逼解
+public:
+    bool isPalindrome(int x) {
+        if (x == 0) return true;
+        else
+        if (x > 0) {
+            int n = log10(x);
+            LL x_reverse = 0;
+            LL tmp = x;
+            EFOR(i, 0, n) {x_reverse = x_reverse * 10 + tmp % 10; tmp /= 10;}
             return (LL)x == x_reverse;
         }
         else return false;
