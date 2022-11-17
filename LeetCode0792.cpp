017365dad97c2936925752c12b397b58b4d05173
Monkey_G <56499092+Monkey-G@users.noreply.github.com>
2022-11-17 14:33:03 +0800

    Update LeetCode0792.cpp

--- LeetCode0792.cpp
+++ LeetCode0792.cpp
@@ -24,3 +24,40 @@ public:
         return ans;
     }
 };
+
+
+#define FOR(i, n) for(int i=0; i < n; ++i)
+
+char*cursor[26][50001];
+int len[26];
+class Solution02 {//网友的神仙解
+public:
+    int numMatchingSubseq(string S, vector<string>& words) {
+        memset(len,0,sizeof(len));//把len中的初值全部初始化为0
+        FOR(i, words.size()){//统计所有word，把它们的指针存进cursor的相应位置
+            int c=words[i][0]-'a';
+            cursor[c][len[c]++]=&words[i][0];
+        }
+        int ans=0;
+        for(char c:S){
+            c-='a';
+            int k=len[c];
+            len[c]=0;
+            for(int i=0;i<k;i++){
+                char*next=cursor[c][i]+1;
+                if(!*next)ans++;
+                else{
+                    int d=*next-'a';
+                    cursor[d][len[d]++]=next;
+                }
+            }
+        }
+        return ans;
+    }
+};
+//IO
+int _IO=[](){
+	ios::sync_with_stdio(0);
+	cin.tie(0); //cout.tie(0);
+	return 0;
+}();
