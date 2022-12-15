6f9be6e5ff4b9751169bfa0d28e4e4251f8d5720
Monkey_G <56499092+Monkey-G@users.noreply.github.com>
2022-12-16 01:35:31 +0800

    Update LeetCode0042.cpp

--- LeetCode0042.cpp
+++ LeetCode0042.cpp
@@ -1,4 +1,4 @@
-class Solution01 {
+class Solution01 {//自己写的四指针
 public:
     int trap(vector<int>& height) {
         int n = height.size();
@@ -19,6 +19,86 @@ public:
             if (height[l2] >= height[r2]) {ans += right_cnt; right_cnt = 0; r2 = l2;}
             else right_cnt += height[r2]-height[l2];
             --l2;
+        }
+        return ans;
+    }
+};
+
+
+
+class Solution02 {//数组实现
+public:
+    int trap(vector<int>& height) {
+        int res = 0;
+        int l[height.size()];
+        int r[height.size()];
+        l[0]=0;
+        r[height.size()-1]=0;
+        for(int i=1;i<height.size()-1;i++){
+            l[i] = l[i-1]>height[i-1] ? l[i-1] : height[i-1];
+        }
+        for(int i=height.size()-2;i>=0;i--){
+            r[i] = r[i+1]>height[i+1] ? r[i+1] : height[i+1];
+        }
+        for(int i=1;i<height.size()-1;i++){
+            int min = l[i]>r[i] ? r[i] : l[i];
+            if(min>height[i]){
+                res+=(min-height[i]);
+            }
+        }
+        return res;
+    }
+
+};
+
+
+class Solution03 {//官方单调栈
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        stack<int> stk;
+        int n = height.size();
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && height[i] > height[stk.top()]) {
+                int top = stk.top();
+                stk.pop();
+                if (stk.empty()) {
+                    break;
+                }
+                int left = stk.top();
+                int currWidth = i - left - 1;
+                int currHeight = min(height[left], height[i]) - height[top];
+                ans += currWidth * currHeight;
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
+
+
+class Solution04 {//官方动态规划
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
+        }
+
+        vector<int> rightMax(n);
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += min(leftMax[i], rightMax[i]) - height[i];
         }
         return ans;
     }
