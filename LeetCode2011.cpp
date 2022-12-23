39cb2a66e1a86eccb7d327b27ff2ebf6cff8ec38
Monkey_G <56499092+Monkey-G@users.noreply.github.com>
2022-12-23 13:02:36 +0800

    Create LeetCode2011.cpp

---
+++ LeetCode2011.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for (string &operation : operations) {
+            if (operation[1] == '+') ++ans;
+            else --ans;
+        }
+        return ans;
+    }
+};
